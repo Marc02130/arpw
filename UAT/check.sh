551c761222e6d8425d621cbfaa5696926432ff81
Marc02130 <marc.breneiser@gmail.com>
2026-09-08 08:41:06 -0400

    uat run

---
+++ UAT/check.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /Users/marcbreneiser/Code/arpw
+exec /Users/marcbreneiser/.nvm/versions/node/v24.14.0/bin/node --check UAT/run-literature-review.mjs
---
+++ UAT/launch-uat.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /Users/marcbreneiser/Code/arpw
+exec /Users/marcbreneiser/.nvm/versions/node/v24.14.0/bin/node UAT/run-literature-review.mjs
